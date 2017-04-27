91f3413c4ba21fe9146c7db0a3fe4a72258e4e0a
André Skoglund Hansen <andre.ok@online.no>
2017-04-27 04:46:15 +0200

    Canvas scales to screen. Some tweaks to basic project setup.

--- static/app/app.js
+++ static/app/app.js
@@ -1,15 +1,15 @@
 var app = angular.module('PlanetEvo', ['ngRoute']);
 
-app.config(['$routeProvider', '$locationProvider',
-    function ($routeProvider, $locationProvider) {
-        $routeProvider.
-            when('/', {
-                templateUrl: 'static/app/home.view.html'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-
-        $locationProvider.html5Mode(true);
-    }
-]);
+//app.config(['$routeProvider', '$locationProvider',
+//    function ($routeProvider, $locationProvider) {
+//        $routeProvider.
+//            when('/', {
+//                templateUrl: 'static/app/home.view.html'
+//            })
+//            .otherwise({
+//                redirectTo: '/'
+//            });
+//
+//        $locationProvider.html5Mode(true);
+//    }
+//]);
---
+++ static/app/world/world.controller.js
@@ -0,0 +1,35 @@
+app.controller('WorldController', function($rootScope, $scope, $window){
+
+    // CONSTRUCTOR
+    var canvas = new fabric.Canvas('worldCanvas');
+    canvas.setBackgroundColor('blue', canvas.renderAll.bind(canvas));
+    canvas.setDimensions({ width: 200, height: 200})
+
+    setDimensionsAccordingToWindowSize();
+
+    $(window).resize(function(){
+        $scope.$apply(function(){
+            setDimensionsAccordingToWindowSize();
+        });
+    });
+
+    // create a rectangle object
+    var rect = new fabric.Rect({
+      left: 100,
+      top: 100,
+      fill: 'red',
+      width: 20,
+      height: 20
+    });
+
+    // "add" rectangle onto canvas
+    canvas.add(rect);
+
+    // EXPOSED ACTIONS FOR HTML
+
+    // PRIVATE FUNCTIONS
+
+    function setDimensionsAccordingToWindowSize() {
+        canvas.setDimensions({ width: $window.innerWidth, height: $window.innerHeight})
+    }
+});
